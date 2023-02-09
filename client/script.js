8f8b7fb3eea68099e5e08774aabafa9248bdb21a
user <tre@Brian.local>
2023-02-09 18:14:07 +0300

    handled POST method bug

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
 
     // fetch data from server -> bot's response
 
-    const response = await fetch('https://codex-v13w.onrender.com', {
+    const response = await fetch('https://codex-v13w.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
